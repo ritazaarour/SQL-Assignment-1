8604a59a5b66680915020c1ed3dafd72e3053f6e
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 19:55:06 -0700

    hw1-1.2.sql

--- hw1-1.2.sql
+++ hw1-1.2.sql
@@ -1,3 +1,4 @@
+#Question: Find all the invoices from the billing country USA, and Canada and sort in descending order by invoice ID.
 SELECT InvoiceId, Total 
 FROM invoices 
 WHERE BillingCountry IN ('USA', 'Canada') 
