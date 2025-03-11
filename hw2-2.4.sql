1e379487e4ed955382c4f392102c91e9d5f912dd
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 20:46:33 -0700

    hw2-2.4.sql

--- hw2-2.4.sql
+++ hw2-2.4.sql
@@ -1,3 +1,6 @@
+/* Question: Find the names of all airlines that ever flew more than 
+5000 flights in one month from California. Return the names of the 
+airlines and the number of flights. Do not return any duplicates. */
 SELECT DISTINCT c.name as name, count(f.fid) as flightcount 
 FROM Carriers c 
 JOIN Flights f ON c.cid=f.carrier_id 
