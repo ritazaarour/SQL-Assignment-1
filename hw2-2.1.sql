468af1e8e76ac883692a4bbb42c95147d5d6f2c1
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 20:44:21 -0700

    hw2-2.1.sql

--- hw2-2.1.sql
+++ hw2-2.1.sql
@@ -1,3 +1,5 @@
+/* Question: Compute the total departure delay of each airline across all flights.
+Name the output columns name and delay, in that order. */
 SELECT c.name as name, sum(f.departure_delay) as delay 
 FROM Carriers c 
 JOIN Flights f ON c.cid=f.carrier_id 
