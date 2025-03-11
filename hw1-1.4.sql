9dd318b0b7736eba13b1bcefd64e5daeb9921f88
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 20:00:05 -0700

    hw1-1.4.sql

--- hw1-1.4.sql
+++ hw1-1.4.sql
@@ -1,4 +1,6 @@
-#Question: Write a query that returns a table consisting of the billing countries and the number of invoices for each country sorted by the country name.
+/* Question: Write a query that returns a table consisting of 
+the billing countries and the number of invoices for each 
+country sorted by the country name. */
 SELECT BillingCountry, count(InvoiceId) 
 FROM invoices 
 GROUP BY BillingCountry 
