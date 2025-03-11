b35ad72f6df5c6099ee9f6f0986cb84e85a978d4
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 20:45:01 -0700

    hw2-2.2.sql

--- hw2-2.2.sql
+++ hw2-2.2.sql
@@ -1,3 +1,5 @@
+/* Question: Find the total capacity of all direct flights between 
+San Diego and San Francisco on July 1th (i.e., SD to SF or SF to SD). */
 SELECT sum(capacity) as totalcapacity 
 FROM Flights 
 WHERE origin_city IN ('San Diego CA', 'San Francisco CA') AND dest_city IN ('San Diego CA', 'San Francisco CA');
