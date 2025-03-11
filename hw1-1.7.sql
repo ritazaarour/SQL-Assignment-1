7cd72b0a4bd0d1018e0c43349081142b3fb4c8a5
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 20:01:51 -0700

    hw1-1.7.sql

--- hw1-1.7.sql
+++ hw1-1.7.sql
@@ -1,3 +1,5 @@
+/* Question: Write a query that returns the artist name 
+and total number of tracks of the Blues bands. */
 SELECT a.Name as artist_name, count(t.TrackId) as total_tracks 
 FROM artists a 
 JOIN albums al ON a.ArtistId=al.ArtistId 
