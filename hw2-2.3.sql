de01ef4101f48480f2e3261fe673d4541f414258
ritazaarour <ritazaarour9@gmail.com>
2025-03-10 20:45:45 -0700

    hw2-2.3.sql

--- hw2-2.3.sql
+++ hw2-2.3.sql
@@ -1,3 +1,7 @@
+/* Question: Write a query that returns the name and 
+the percentage of canceled flights out of San Diego for 
+all the airlines that more than 1% of their flights out of San Diego were canceled. 
+Order the results by the percentage of canceled flights in ascending order. */ 
 SELECT c.name as name, sum(f.canceled)*100/count(f.canceled) as percent F
 ROM Carriers c 
 JOIN Flights f ON c.cid=f.carrier_id 
